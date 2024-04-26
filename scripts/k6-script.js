563368190b021bc598d6b4881f686aef67890214
nilendra82 <krnilendra@gmail.com>
2024-04-27 02:11:12 +0530

    Added distribution load test with same script

--- scripts/k6-script.js
+++ scripts/k6-script.js
@@ -1,14 +1,26 @@
 import { check } from 'k6';
 import http from 'k6/http';
 
+const usEast1VUs = __ENV.US_EAST_1_VUS || 30;
+const usEast2VUs = __ENV.US_EAST_2_VUS || 70;
+
 export const options = {
-    vus: 70,
-    duration: '1m',
-  };
+  vus: usEast1VUs + usEast2VUs, // Total VUs
+  duration: '1m',
+};
 
 export default function () {
-  const res = http.get('http://test.k6.io/');
-  check(res, {
-    'is status 200': (r) => r.status === 200,
-  });
+  if (__VU <= usEast1VUs) {
+    // VUs for us-east-1
+    const res = http.get('http://test.k6.io/');
+    check(res, {
+      'is status 200': (r) => r.status === 200,
+    });
+  } else {
+    // VUs for us-east-2
+    const res = http.get('http://test.k6.io/');
+    check(res, {
+      'is status 200': (r) => r.status === 200,
+    });
+  }
 }
