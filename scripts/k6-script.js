13442334ba648f0c3f97f6e94e91fb759ab0a866
nilendra82 <krnilendra@gmail.com>
2024-04-27 14:19:24 +0530

    Added distribution load test with same script again

--- scripts/k6-script.js
+++ scripts/k6-script.js
@@ -6,22 +6,21 @@ const usEast1VUs = parseInt(__ENV.US_EAST_1_VUS) || 30;
 const usEast2VUs = parseInt(__ENV.US_EAST_2_VUS) || 70;
 
 export const options = {
-  vus: usEast1VUs + usEast2VUs, // Total VUs
+  // Total duration will be the same for all VUs
   duration: '1m',
 };
 
 export default function () {
-  if (__VU < usEast1VUs) { // Check if current VU is less than usEast1VUs
-    // VUs for us-east-1
-    const res = http.get('http://test.k6.io/');
-    check(res, {
-      'is status 200': (r) => r.status === 200,
-    });
-  } else {
-    // VUs for us-east-2
-    const res = http.get('http://test.k6.io/');
-    check(res, {
-      'is status 200': (r) => r.status === 200,
-    });
-  }
+  // Calculate the number of VUs for each region based on environment variables
+  const totalVUs = usEast1VUs + usEast2VUs;
+  const ratioUsEast1 = usEast1VUs / totalVUs;
+
+  // Determine the number of VUs for this instance
+  const myVUs = Math.floor(totalVUs * ratioUsEast1);
+
+  // Perform checks and HTTP requests
+  const res = http.get('http://test.k6.io/');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
 }
