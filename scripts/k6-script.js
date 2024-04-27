efe12ba5b6e6acd8afb558001b053e227098c69a
nilendra82 <krnilendra@gmail.com>
2024-04-27 16:19:15 +0530

    Added distribution load test with same script again

--- scripts/k6-script.js
+++ scripts/k6-script.js
@@ -1,31 +1,13 @@
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 import http from 'k6/http';
 
-// Read environment variables for the number of VUs for each region
-const usEast1VUs = parseInt(__ENV.US_EAST_1_VUS) || 30;
-const usEast2VUs = parseInt(__ENV.US_EAST_2_VUS) || 70;
-
-export const options = {
-  vus: usEast1VUs + usEast2VUs, // Total VUs
+export let options = {
+  vus: __ENV.US_EAST_1_VUS || __ENV.US_EAST_2_VUS || 1, // default to 1 if the env vars are not set
   duration: '1m',
 };
 
 export default function () {
-  if (__VU < usEast1VUs) { // Check if current VU is less than usEast1VUs
-    // VUs for us-east-1
-    console.log('Current VU for us-east-1', __VU);
-    console.log('VU for us-east-1', usEast1VUs);
-    const res = http.get('http://test.k6.io/');
-    check(res, {
-      'is status 200': (r) => r.status === 200,
-    });
-  } else {
-    // VUs for us-east-2
-    console.log('Current VU for us-east-2', __VU);
-    console.log('VU for us-east-2', usEast2VUs);
-    const res = http.get('http://test.k6.io/');
-    check(res, {
-      'is status 200': (r) => r.status === 200,
-    });
-  }
+  let res = http.get('http://test.loadimpact.com');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
 }
