8ed42e6373d6b13b4c8a4c328f71622284dc2c44
nilendra82 <krnilendra@gmail.com>
2024-04-30 22:09:34 +0530

    Added distribution load test with same script reagain

--- scripts/k6-script.js
+++ scripts/k6-script.js
@@ -8,9 +8,9 @@ let VUS = 10; // Default number of VUs
 
 // Set the number of VUs based on the region
 if (REGION === 'us-east-1') {
-    VUS = 80; // Set the number of VUs for us-east-1
+    VUS = 40; // Set the number of VUs for us-east-1
 } else if (REGION === 'us-east-2') {
-    VUS = 20; // Set the number of VUs for us-east-2
+    VUS = 60; // Set the number of VUs for us-east-2
 } // Add more conditions as needed for other regions
 
 export const options = {
