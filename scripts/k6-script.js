a92dd00616db3b2064913c75b7ca669598d323f5
nilendra82 <krnilendra@gmail.com>
2024-05-27 23:50:13 +0530

    Deleted unwanted files

--- scripts/k6-script.js
+++ scripts/k6-script.js
@@ -2,15 +2,15 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 // Get the region from the environment variable
-const REGION = __ENV.REGION || 'abcd'; // Default to 'us-east-1' if not provided
+const REGION = __ENV.REGION || 'us-east-1'; // Default to 'us-east-1' if not provided
 
 let VUS = 10; // Default number of VUs
 
-// Set the number of VUs based on the region
+// Set the number of VUs based on the region 
 if (REGION === 'us-east-1') {
-    VUS = 40; // Set the number of VUs for us-east-1
+    VUS = 70; // Set the number of VUs for us-east-1
 } else if (REGION === 'us-east-2') {
-    VUS = 60; // Set the number of VUs for us-east-2
+    VUS = 30; // Set the number of VUs for us-east-2
 } // Add more conditions as needed for other regions
 
 export const options = {
@@ -19,7 +19,7 @@ export const options = {
 };
 
 export default function () {
-    const response = http.get('http://example.com');
+    const response = http.get('http://test.k6.io/');
     check(response, {
         'status is 200': (r) => r.status === 200,
     });
